f05b93f61a6ebd039fbf445fd36ed8cba2be50c7
Subhranil <subhranilbanerjee485100@gmail.com>
2024-05-05 22:48:45 +0530

    second EXercise  done

--- _02_Using_Variables/src/Exercise.java
+++ _02_Using_Variables/src/Exercise.java
@@ -3,7 +3,15 @@ package _02_Using_Variables.src;
 
 public class Exercise {
  
-   
+        public static void main(String args[]){
+
+                int num1=10;
+                int num2=5;
+                System.out.println("add = "+(num1+num2));
+                System.out.println("sub = "+(num1-num2));
+                System.out.println("mul = "+(num1*num2));
+                System.out.println("div = "+(num1/num2));
+        }
 
         /*
 
