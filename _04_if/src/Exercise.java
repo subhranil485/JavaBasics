1acf81826f078f83f9f4187e9a7c09ef34766a71
Subhranil <subhranilbanerjee485100@gmail.com>
2024-05-08 23:04:17 +0530

    if done

--- _04_if/src/Exercise.java
+++ _04_if/src/Exercise.java
@@ -1,10 +1,32 @@
 package _04_if.src;
 
-
+import java.util.Scanner;
 
 public class Exercise {
     public static void main(String[] args) {
-
+		
+		int a,b,c,temp,larg;
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter ithe 1st no: ");
+		a = sc.nextInt();
+		System.out.println("Enter ithe 2nd no: ");
+		b = sc.nextInt();
+		System.out.println("Enter ithe 3rd no: ");
+		c = sc.nextInt();
+		if (a>b && a>c) 
+			{
+			System.out.println("The largest no. is: "+a);
+			}
+		else if (b>c && b>a) {
+			System.out.println("The largest no. is: "+b);
+			} 
+		else if (c>a && c>b) {
+			System.out.println("The largest no. is: "+c);	
+		}
+	
+	}
+	
+}
 /*		Take three numbers from the user and print the greatest number. Go to the editor
 		Test Data
 		Input the 1st number: 25
@@ -12,6 +34,3 @@ public class Exercise {
 		Input the 3rd number: 87
 		Expected Output :
 		The greatest: 87*/
-
-    }
-}
