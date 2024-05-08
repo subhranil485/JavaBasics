dabfc5a36807adfd076f6ebbf1049bf5330b0c65
Subhranil <subhranilbanerjee485100@gmail.com>
2024-05-08 23:51:50 +0530

    whole done

--- _05_While_Loops/src/Exercise.java
+++ _05_While_Loops/src/Exercise.java
@@ -1,7 +1,23 @@
 package _05_While_Loops.src;
 
-
+import java.util.Scanner;
 
 public class Exercise {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the range: ");
+        a=sc.nextInt();
+        int first=0 , second =1 , sum;
+        sum = first+second ; 
+    System.out.print(first+" "+second);
+        while (sum<=a) {
+            System.out.print(" " +sum+ " ");
+            first = second;
+            second=sum;
+            sum= first+second;                                  
+
+        }
+    }
     //TODO print all  Fibonacci number from 1 to 1000
 }
