79caa9003101995ce6d20704425c73e35a775a45
Subhranil <subhranilbanerjee485100@gmail.com>
2024-05-10 01:13:51 +0530

    class obj(1) done

--- _11_Classes_and_Objects/src/Test.java
+++ _11_Classes_and_Objects/src/Test.java
@@ -1,7 +1,26 @@
 package _11_Classes_and_Objects.src;
-
+import java.util.*;
+class cake{
+    String name;
+    String color;
+    String flavour;
+    int pound;
+    int price;
+}
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        cake cake1 = new cake();
+        cake1.name = "Raspberry Ripple Cake";
+        cake1.color = "Queen Pink";
+        cake1.flavour = "Raspberry";
+        cake1.pound = 5;
+        cake1.price = 4250;
+
+        System.out.println("Name: "+cake1.name);
+        System.out.println("Name: "+cake1.color);
+        System.out.println("Name: "+cake1.flavour);
+        System.out.println("Name: "+cake1.pound);
+        System.out.println("Name: "+cake1.price);
+
     }
 }
